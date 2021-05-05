7a178957aa6302ec0babd170714b1de4bd9ee491
Vyas Raina <sayvaniar@gmail.com>
2021-05-05 12:40:34 +0100

    plt

--- eval_hierarchical.py
+++ eval_hierarchical.py
@@ -14,6 +14,7 @@ import torch
 import torch.nn as nn
 from tools import calculate_mse
 import numpy as np
+import matplotlib.pyplot as plt
 
 def apply_hierarchal(preds_stage1, preds_stage2, thresh=4.0):
     preds = []
